7cf6d04ace7a48069106a0a1f9d8f5bc4a7a530b
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-03-21 13:17:51 +0100

    Update inowas.py

--- inowas.py
+++ inowas.py
@@ -1,11 +1,16 @@
 import json
+import os
 import sys
 from InowasFlopyAdapter.InowasFlopyAdapter import InowasFlopyAdapter
 
 
 def main():
-    filename = sys.argv[1]
+    filename = os.path.realpath(sys.argv[1])
     print('This script loads file: %s' % filename)
+
+    dirname = os.path.dirname(filename)
+    os.chdir(dirname)
+    print('Change to directory: %s' % print(dirname))
 
     with open(filename) as data_file:
         content = json.load(data_file)
