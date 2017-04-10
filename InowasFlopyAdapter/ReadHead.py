6df79494e68e59c7ba4aafa95ee9b6059f53e854
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-04-10 23:31:34 +0200

    Update Adapter

--- InowasFlopyAdapter/ReadHead.py
+++ InowasFlopyAdapter/ReadHead.py
@@ -13,22 +13,23 @@ class ReadHead:
         pass
 
     def read_times(self):
-        if not self._filename:
+        try:
+            heads = bf.HeadFile(filename=self._filename, precision='single')
+            return heads.get_times()
+        except:
             return []
 
-        heads = bf.HeadFile(filename=self._filename, precision='single')
-        return heads.get_times()
 
     def read_layer(self, totim, layer):
-        if not self._filename:
+        try:
+            heads = bf.HeadFile(filename=self._filename, precision='single')
+            return heads.get_data(totim=totim, mflay=layer)
+        except:
             return []
-
-        heads = bf.HeadFile(filename=self._filename, precision='single')
-        return heads.get_data(totim=totim, mflay=layer)
 
     def read_ts(self, layer, row, column):
-        if not self._filename:
+        try:
+            heads = bf.HeadFile(filename=self._filename, precision='single')
+            return heads.get_ts(idx=(layer, row, column))
+        except:
             return []
-
-        heads = bf.HeadFile(filename=self._filename, precision='single')
-        return heads.get_ts(idx=(layer, row, column))
