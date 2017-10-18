7d697d368c9f10deec48154fea831886c5d002c8
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-10-18 11:16:04 +0200

    Add optional interpolation to extract-data-method

--- InowasGeoProcessing/InowasGeoProcessing.py
+++ InowasGeoProcessing/InowasGeoProcessing.py
@@ -23,6 +23,21 @@ class InowasGeoProcessing:
 
         if self._data['method'] == 'extractRasterData':
             print('Method: extractRasterData')
+
+            if 'parameters' not in self._data:
+                return dict(
+                    status_code=422,
+                    body="Invalid argument exception, key: 'parameters' not given."
+                )
+
+            parameters = self._data['parameters']
+
+            if 'file' not in parameters:
+                return dict(
+                    status_code=422,
+                    body="Invalid argument exception, key: 'file' not found in parameters."
+                )
+
             filename = os.path.join(self._datafolder, self._data['parameters']['file'])
             file = RasterFile(filename=filename)
 
@@ -32,11 +47,33 @@ class InowasGeoProcessing:
                     body="File not found or not valid."
                 )
 
+            width = False
+            if 'width' in parameters:
+                width = parameters['width']
+
+            height = False
+            if 'height' in parameters:
+                height = parameters['height']
+
+            available_interpolation_methods = [
+                'nearest', 'linear', 'cubic'
+            ]
+
+            method = available_interpolation_methods[0]
+            if 'method' in parameters:
+                method = parameters['method']
+                if parameters['method'] not in available_interpolation_methods:
+                    return dict(
+                        status_code=422,
+                        body="Invalid argument exception, interpolation method: {} not available. "
+                             "Available methods are {}.".format(method, ', '.join(available_interpolation_methods))
+                    )
+
             return dict(
                 status_code=200,
                 body={
                     'metadata': file.get_metadata(),
-                    'data': file.get_data()
+                    'data': file.get_data(width=width, height=height, method=method)
                 }
             )
 
--- InowasGeoProcessing/RasterFile.py
+++ InowasGeoProcessing/RasterFile.py
@@ -1,4 +1,6 @@
 import gdal
+import numpy as np
+from scipy.interpolate import griddata
 
 
 class RasterFile:
@@ -8,15 +10,19 @@ class RasterFile:
 
     def __init__(self, filename):
         self._filename = filename
+
+    def open_file(self):
         self._dataset = gdal.Open(self._filename, gdal.GA_ReadOnly)
 
     def is_valid(self):
+        self.open_file()
         if type(self._dataset) is not gdal.Dataset:
             return False
 
         return True
 
     def get_metadata(self):
+        self.open_file()
         if not self.is_valid():
             raise FileNotFoundError('File not valid.')
 
@@ -35,13 +41,30 @@ class RasterFile:
 
         return metadata
 
-    def get_data(self):
+    def get_data(self, width=False, height=False, method='nearest'):
+        self.open_file()
         if not self.is_valid():
             raise FileNotFoundError('File not valid.')
 
         data = []
         for iBand in range(1, self._dataset.RasterCount+1):
             band = self._dataset.GetRasterBand(iBand)
-            data.append(band.ReadAsArray().tolist())
+            band_data = band.ReadAsArray()
+
+            if width and height:
+                band_data = self.interpolate(data2d=band_data, width=width, height=height, method=method)
+
+            data.append(band_data.tolist())
 
         return data
+
+    @staticmethod
+    def interpolate(data2d, width, height, method):
+
+        if len(data2d) == height and len(data2d[0]) == width:
+            return data2d
+
+        grid_x, grid_y = np.mgrid[0:len(data2d[0]):complex(height), 0:len(data2d):complex(width)]
+        points = np.indices((len(data2d), len(data2d[0]))).T.reshape(-1, 2)
+        values = data2d.flatten('F')
+        return griddata(points, values, (grid_x, grid_y), method=method)
