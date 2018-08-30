3e0454e45927fe33052b2795fb96ec912162516e
aybulatf <aybulat.f@gmail.com>
2018-08-30 21:08:41 +0500

    Fix validate_spd

--- Optimization/Validator.py
+++ Optimization/Validator.py
@@ -7,28 +7,28 @@ def validate_spd(data):
         return spd_dict
     #convert optimization objects' data
     for idx, _object in enumerate(data["optimization"]["objects"]):
-        if type(_object["flux"]) == list:
+        if "flux" in _object and type(_object["flux"]) == list:
             data["optimization"]["objects"][idx]["flux"] = convert_list_to_dict(_object["flux"])
-        if type(_object["concentration"]) == list:
+        if "concentration" in _object and type(_object["concentration"]) == list:
             data["optimization"]["objects"][idx]["concentration"] = convert_list_to_dict(_object["concentration"])
 
     #convert modflow spd data
-    for package_name, package_data in data["data"]["mf"].items():
-        try:
-            if type(package_data["stress_period_data"]) == list:
-                data["data"]["mf"][package_name]["stress_period_data"]\
-                = convert_list_to_dict(package_data["stress_period_data"])
-        except KeyError:
-            pass
+    # for package_name, package_data in data["data"]["mf"].items():
+    #     try:
+    #         if type(package_data["stress_period_data"]) == list:
+    #             data["data"]["mf"][package_name]["stress_period_data"]\
+    #             = convert_list_to_dict(package_data["stress_period_data"])
+    #     except (TypeError, KeyError):
+    #         pass
     
-    #convert mt3d spd data
-    for package_name, package_data in data["data"]["mt"].items():
-        try:
-            if type(package_data["stress_period_data"]) == list:
-                data["data"]["mt"][package_name]["stress_period_data"]\
-                = convert_list_to_dict(package_data["stress_period_data"])
-        except KeyError:
-            pass
+    # #convert mt3d spd data
+    # for package_name, package_data in data["data"]["mt"].items():
+    #     try:
+    #         if type(package_data["stress_period_data"]) == list:
+    #             data["data"]["mt"][package_name]["stress_period_data"]\
+    #             = convert_list_to_dict(package_data["stress_period_data"])
+    #     except (TypeError, KeyError):
+    #         pass
     
     return data
         
